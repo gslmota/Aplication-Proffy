8256cb3a77aa51fb8ad54497430ad7950ceb0d53
Gabriel Sávio <gabrielsavio81@gmail.com>
2020-08-13 10:14:54 -0300

    up

--- NLW/scripts/addField.js
+++ NLW/scripts/addField.js
@@ -8,8 +8,9 @@ function cloneField(){
     const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
     // clean
     const fields = document.querySelectorAll('input')
-    fields[0].value = ""
-    fields[1].value = ""
+    fields.forEach(function(field){
+        field.value = ""
+    })
     // pagina
     document.querySelector('#schedule-items').appendChild(newFieldContainer)
 }
